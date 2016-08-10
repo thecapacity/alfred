95863dee93bb9857db24e9f23caf341b8ad6c149
jay <thecapacity@gmail.com>
2016-08-09 20:48:53 -0400

    Base Test Class

---
+++ alfred_tests.py
@@ -0,0 +1,20 @@
+import os
+import alfred
+import unittest
+import tempfile
+
+""" Guidance from: http://flask.pocoo.org/docs/0.11/testing/#testing """
+
+class Alfred(unittest.TestCase):
+
+    def setUp(self):
+        self.app = alfred.app.test_client()
+
+        with alfred.app.app_context():
+            alfred.init_db()
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
