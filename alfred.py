5ca76c1254920f488e4350fe253e88d8f6f005b7
jay <thecapacity@gmail.com>
2016-08-16 06:57:09 -0400

    Initial Config File
    Will auto-populate secret key if necessary

--- alfred.py
+++ alfred.py
@@ -7,9 +7,29 @@ from flask import Flask, flash, jsonify, render_template, request, session, redi
 
 app = Flask(__name__)
 
+def rand_ascii(size=24, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 @app.route("/")
 def index():
     return render_template ('index.html')
 
 if __name__ == "__main__":
+
+    config = { }
+
+    try:
+        with open('secret.json', 'r') as config_file:
+            config = json.load(config_file)
+
+        print "Config Loaded:", config
+        app.secret_key = config['app_secret_key']
+    except:
+        app.secret_key = rand_ascii()
+        print "Auto Generated Secret:", app.secret_key
+
+        config['app_secret_key'] = app.secret_key
+        with open('secret.json', 'w') as config_file:
+            json.dump(config, config_file)
+
     app.run()
