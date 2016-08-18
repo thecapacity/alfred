e971061435eec55bd687dd567f059f6a7b2af508
jay <thecapacity@gmail.com>
2016-08-17 21:38:00 -0400

    Create Sample Entry

---
+++ sample_entries.sql
@@ -0,0 +1,14 @@
+INSERT INTO links ( title, url, time, tags, comment )
+    VALUES (
+        "Watchdog — watchdog v0.5.4 documentation",
+        "http://pythonhosted.org/watchdog/", 
+        "August 16, 2016", 
+        "python cli filesystem sysprog", 
+        "watch filesystem for changes - library and cli"
+    );
+
+
+
+
+
+
