3748ef2b4caa36160fac87bbc540279481e9b42c
jay <thecapacity@gmail.com>
2016-08-17 21:41:00 -0400

    Fix Typo
    “”” should have been earlier

--- alfred.py
+++ alfred.py
@@ -36,12 +36,11 @@ def query_db(query, args=(), one=False):
         print 'No such user'
     else:
         print the_username, 'has the id', user['user_id']
-
-        cur = get_db().execute(query, args)
-        rv = cur.fetchall()
-        cur.close()
-        return (rv[0] if rv else None) if one else rv
     """
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route('/entries', methods=['GET', 'POST'])
 def entries():
