64c17deffb65e394016c20ac00eca9d87fb380f8
jay <thecapacity@gmail.com>
2016-08-17 21:19:28 -0400

    DB Init Function

--- alfred.py
+++ alfred.py
@@ -30,6 +30,12 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
 @app.route('/logout', methods=['POST'])
 def logout():
     error = None
