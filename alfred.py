e79ade5104c1fefd0d2fdcb21861314ea60127d1
jay <thecapacity@gmail.com>
2016-08-28 16:06:22 -0400

    Removed Separate Function for init_db()

--- alfred.py
+++ alfred.py
@@ -97,17 +97,15 @@ def close_db(error):
     if hasattr(g, 'sqlite_db') and os.path.isfile( app.config['DATABASE'] ):
         g.sqlite_db.close()
 
-def init_db():
+@app.cli.command('initdb')
+def initdb_command():
+    """ Initializes the database.
+    """
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
 
-@app.cli.command('initdb')
-def initdb_command():
-    """ Initializes the database.
-    """
-    init_db()
     app.logger.debug('Initialized the database.')
 
 @app.cli.command('dropdb')
