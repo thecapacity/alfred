7ba8d905fd341a9d2ba6047ae745ba7b55390f85
jay <thecapacity@gmail.com>
2016-09-10 10:14:51 -0400

    Add privacy flag

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 drop table if exists links;
 create table links (
       id integer primary key autoincrement,
+      private int not null default 0,
       title text not null,
       url text not null,
       time text not null,
